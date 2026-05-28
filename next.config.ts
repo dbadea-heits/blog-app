1216fa81fba8bbc96cc2b2af4a1ee8aad2a6ee85
Dan Badea <v-dbadea@digitalocean.com>
2026-05-28 13:46:39 +0300

    Static homepage from markdown

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
+import createMDX from "@next/mdx"
+
+const withMDX = createMDX({
+  extension: /\.(md|mdx)$/,
+})
 
 const nextConfig: NextConfig = {
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   turbopack: {
     root: __dirname,
   },
-};
+}
 
-export default nextConfig;
+export default withMDX(nextConfig)
